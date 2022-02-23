49dbe02d0c3fdbbb6ff5f394b902d09f6210d7f6
vivekgupta6534294 <vivekgupta6534294@gmail.com>
2022-02-23 12:19:35 +0530

    code added

---
+++ PrintFibonacciNumbersTillN.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class PrintFibonacciNumbersTillN {
+
+    public static void main(String[] args) {
+        // write your code here
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int pre2 = 0;
+        System.out.println("0");
+        int pre1 = 1;
+        System.out.println("1");
+        for (int i = 2; i < num; i++) {
+            int curr = pre1 + pre2;
+            pre2 = pre1;
+            pre1 = curr;
+            System.out.println(curr);
+            sc.close();
+        }
+    }
+}
+// Sample Input
+
+// 10
+
+// Sample Output
+// 0
+// 1
+// 1
+// 2
+// 3
+// 5
+// 8
+// 13
+// 21
+// 34
