d078fe98015deaaa27034abb1984ec3309b56da6
vivek gupta <vivekgupta6534294@gmail.com>
2022-04-22 22:51:30 +0530

    "GetMazePaths.java done "

---
+++ recursionAndBacktrackingForBeginners/GetMazePaths.java
@@ -0,0 +1,46 @@
+package recursionAndBacktrackingForBeginners;
+
+import java.io.*;
+import java.util.*;
+public class GetMazePaths {
+    public static void main(String[] args) throws Exception {
+        Scanner scn=new Scanner(System.in);
+        int sr=scn.nextInt();
+        int sc=scn.nextInt();
+        int dr=scn.nextInt();
+        int dc=scn.nextInt();
+        ArrayList<String> ans=getMazePaths(sr, sc, dr, dc);
+        System.out.println(ans);
+    }
+
+    // sr - source row
+    // sc - source column
+    // dr - destination row
+    // dc - destination column
+    public static ArrayList<String> getMazePaths(int sr, int sc, int dr, int dc) {
+        if(sr==dr && sc==dc){
+            ArrayList<String> base=new ArrayList<>();
+            base.add("");
+            return base;//[""]
+        }
+        if(sr>dr||sc>dc){
+            ArrayList<String> base=new ArrayList<>();
+            return base;//[]
+        }
+        //Horizontal path 
+        ArrayList<String> hpath=getMazePaths(sr, sc+1, dr, dc);
+        //Vertical Path
+        ArrayList<String> vpath=getMazePaths(sr+1, sc, dr, dc);
+        
+        // Result : 
+        ArrayList<String> myPath=new ArrayList<>();
+        for(String path: hpath){
+            myPath.add("h"+path);
+        }
+        for(String path : vpath) {
+            myPath.add("v"+path);
+        }
+        return myPath;
+    }
+
+}
