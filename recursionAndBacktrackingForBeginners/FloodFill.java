fcce95fdb0d6adaf902627c196f4bb2f38867dd3
vivek gupta <vivekgupta6534294@gmail.com>
2022-05-06 22:42:58 +0530

    modified

--- recursionAndBacktrackingForBeginners/FloodFill.java
+++ recursionAndBacktrackingForBeginners/FloodFill.java
@@ -1,8 +1,7 @@
-package recursionAndBacktrackingForBeginners;
 import java.io.*;
 import java.util.*;
 
-public class FloodFill {
+public class Main {
 
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
@@ -14,24 +13,24 @@ public class FloodFill {
                 arr[i][j] = scn.nextInt();
             }
         }
-        floodfill(arr, 0, 0, "", new boolean[n][m]);
+        floodfill(arr, 0, 0, "",new boolean[n][m]);
     }
     
     // asf -> answer so far
     public static void floodfill(int[][] maze, int sr, int sc, String asf,boolean visited[][]) {
-        if(sr==maze.length-1  && sc==maze[0].length-1){
+        if(sr == maze.length-1 && sc == maze[0].length-1){ // valid
             System.out.println(asf);
             return;
         }
-        if(sr<0|| sc<0 ||sr>maze.length-1||sc>=maze.length-1||maze[sr][sc]==1||visited[sr][sc]){
+        if(sr < 0 || sc < 0 || sr >= maze.length || sc >= maze[0].length || maze[sr][sc] == 1 || visited[sr][sc]){ // invalid
             return;
         }
-        visited[sr][sc]=true;
-        floodfill(maze, sr-1, sc, asf+"t", visited);
-        floodfill(maze, sr, sc-1, asf+"l", visited);
-        floodfill(maze, sr+1, sc, asf+"d", visited);
-        floodfill(maze, sr, sc+1, asf+"r", visited);
-        visited[sr][sc]=false;
-    
+
+        visited[sr][sc] = true; // mark 
+        floodfill(maze,sr-1,sc,asf+"t",visited);
+        floodfill(maze,sr,sc-1,asf+"l",visited);
+        floodfill(maze,sr+1,sc,asf+"d",visited);
+        floodfill(maze,sr,sc+1,asf+"r",visited);
+        visited[sr][sc] = false; /// unmark
     }
 }
