1d9271a4f82cc643c9aedbcdd6eada7a3cda4b3a
vivek gupta <vivekgupta6534294@gmail.com>
2022-06-15 22:47:27 +0530

    display

--- genericTree/Construct.java
+++ genericTree/Construct.java
@@ -32,8 +32,23 @@ public class Construct{
         }
         return root;
     }
+    public static void display(Node root){
+        // No need for base case
+        System.out.print(root.data+" -> ");
+        // for(int i=0;i<root.children.size();i++){
+        //     display(root.children.get(i));
+        // }
+        for(Node child:root.children){
+            System.out.print(child.data+" ");
+        }
+        System.out.println(".");
+        for(Node child:root.children){
+            display((child));
+        }
+    }
     public static void main(String args[]){
         Integer []arr = {10, 20, 50, null, 60, null, null, 30, 70, 90, null, 100, null, 110, null, null, null, 40, 80, null, null, null};
         Node root = construct(arr);
+        display(root);
     }
 }
