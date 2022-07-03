a4dc569b05ab63b4634f7a3593b61ee5e4d594dd
vivek gupta <vivekgupta6534294@gmail.com>
2022-07-03 23:12:31 +0530

    Highest Frequency Character

--- hashMapAndHeap/HighestFrequencyCharacter.java
+++ hashMapAndHeap/HighestFrequencyCharacter.java
@@ -3,33 +3,41 @@ package hashMapAndHeap;
 import java.io.*;
 import java.util.*;
 
+// Example
+// Sample Input
+
+// zmszeqxllzvheqwrofgcuntypejcxovtaqbnqyqlmrwitc
+
+// Sample Output
+// q
+
 public class HighestFrequencyCharacter {
     public static char maxFreqCh(String str){
-        HashMap<Character,Integer> hm = new HashMap<>();
-        for(int idx = 0 ; idx < str.length() ; idx++){
-            char ch = str.charAt(idx);
-            if(hm.containsKey(ch)){
-                hm.put(ch,hm.get(ch)+1); //  update
-            }else{
-                hm.put(ch, 1); // add
-            }
-        }
-
-        int maxFreq = 0;
-        char mfch = ' ';
-        for(Character ch : hm.keySet()){
-            if(maxFreq < hm.get(ch)){
-                maxFreq = hm.get(ch);
-                mfch = ch;
-            }
-        }
-        return mfch;
-    }
-    public static void main(String args[]){
-        Scanner scn = new Scanner(System.in);
-        String str = scn.nextLine();
-        char ch = maxFreqCh(str);
-        System.out.println(ch);
-    }
-
-}
+         HashMap<Character,Integer> hm = new HashMap<>();
+         for(int idx = 0 ; idx < str.length() ; idx++){
+             char ch = str.charAt(idx);
+             if(hm.containsKey(ch)){
+                 hm.put(ch,hm.get(ch)+1); //  update
+             }else{
+                 hm.put(ch, 1); // add
+             }
+         }
+ 
+         int maxFreq = 0;
+         char mfch = ' ';
+         for(Character ch : hm.keySet()){
+             if(maxFreq < hm.get(ch)){
+                 maxFreq = hm.get(ch);
+                 mfch = ch;
+             }
+         }
+         return mfch;
+     }
+     public static void main(String args[]){
+         Scanner scn = new Scanner(System.in);
+         String str = scn.nextLine();
+         char ch = maxFreqCh(str);
+         System.out.println(ch);
+     }
+ 
+ }
