91792a8d613306486e078e321bee955246bf6d9c
vivek gupta <vivekgupta6534294@gmail.com>
2022-07-16 19:57:05 +0530

    Climb Stair

--- dynamicProgrammingAndGreedyForBeginners/ClimbStairs.java
+++ dynamicProgrammingAndGreedyForBeginners/ClimbStairs.java
@@ -1,5 +1,28 @@
 package dynamicProgrammingAndGreedyForBeginners;
-
+import java.util.*;
 public class ClimbStairs {
-    
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println(rec(n));
+        System.out.println(recMemo(n,new int[n+1]));
+    }
+    public static int rec(int n){
+        if(n==0) return 1;
+        int ans=0;
+        for(int jump=1;jump<=3 &&n-jump>=0;jump++){
+            ans+=rec(n-jump);
+        }
+        return ans;
+    }
+    public static int recMemo(int n,int []dp){
+        if(n==0) return 1;
+        if(dp[n]!=0) return dp[n];
+        int ans=0;
+        for(int jump=1;jump<=3 &&n-jump>=0;jump++){
+            ans+=recMemo(n-jump,dp);
+        }
+        return dp[n]=ans;
+    }
+ 
 }
