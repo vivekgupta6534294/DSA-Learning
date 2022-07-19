7f5ce7275f55722535cf078dec8f078b7dffaf93
vivek gupta <vivekgupta6534294@gmail.com>
2022-07-20 00:26:42 +0530

    MIn Cost in Maze Traversal

--- dynamicProgrammingAndGreedyForBeginners/MinCostInMazeTraversal.java
+++ dynamicProgrammingAndGreedyForBeginners/MinCostInMazeTraversal.java
@@ -1,44 +1,41 @@
-package dynamicProgrammingAndGreedyForBeginners;
 import java.io.*;
 import java.util.*;
 
-public class MinCostInMazeTraversal {
+public class Main {
 
     public static void main(String[] args) throws Exception {
-        // write your code here
-        Scanner sc=new Scanner(System.in);
-        int m =sc.nextInt();
-        int n=sc.nextInt();
-        int cost[][]=new int[m][n];
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                cost[i][j]=sc.nextInt();
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int m = scn.nextInt();
+        int cost[][] = new int[n][m];
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < m ; j++){
+                cost[i][j] = scn.nextInt();
             }
         }
-        System.out.println(find(0,0,m-1,n-1,cost));
+
+        System.out.println(minCost(cost));
     }
-    public static int find (int sr,int sc, int dr, int dc, int cost[][]){
 
-        int dp[][]=new int[dr][dc];
+    public static int minCost(int cost[][]){
+        int nr = cost.length , nc = cost[0].length;
+        int mem[][] = new int[nr][nc];
 
-        for(int i=dr-1;i>=0;i--){
-            for(int j=dc-1;j>=0;j--){
-                if(i==dr && j== dc){
-                    dp[i][j]=cost[i][j];
-                }
-                else if(i==dr){
-                    dp[i][j]=cost[i][j]+cost[i+1][j];
-                }else if(j==dc){
-                    dp[i][j]=cost[i][j]+cost[i][j+1];
+        for(int r = nr-1 ; r >= 0 ; r--){
+            for(int c = nc-1; c >= 0 ; c--){
+                if(r == nr-1 && c == nc-1){
+                    mem[r][c] = cost[r][c];
+                }else if(r == nr-1){
+                    mem[r][c] = mem[r][c+1] + cost[r][c];
+                }else if(c == nc-1){
+                    mem[r][c] = mem[r+1][c] + cost[r][c];
                 }else{
-
-                    dp[i][j]=Math.min(dp[i+1][j],dp[i][j+1])+cost[i][j];
+                    mem[r][c] = Math.min(mem[r][c+1],mem[r+1][c])+cost[r][c];
                 }
             }
         }
-        return dp[0][0];
 
-
+        return mem[0][0];
     }
 
 }
