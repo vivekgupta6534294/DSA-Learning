786dd7deb2e2fe29eacedb20159c58494c843ea8
vivek gupta <vivekgupta6534294@gmail.com>
2022-07-20 00:25:43 +0530

    Climb Stair With Variable Jump

--- dynamicProgrammingAndGreedyForBeginners/ClimbStairsWithVariableJumps.java
+++ dynamicProgrammingAndGreedyForBeginners/ClimbStairsWithVariableJumps.java
@@ -1,37 +1,49 @@
-package dynamicProgrammingAndGreedyForBeginners;
+import java.io.*;
 import java.util.*;
-public class ClimbStairsWithVariableJumps {
+
+public class Main {
+
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        int moves[]=new int[n];
-        for(int i=0;i<n;i++){
-            moves[i]=sc.nextInt();
-        }  
-        // System.out.println(ClimbStairsWithVariableJumps_rec(0, n, moves));   
-        int []dp=new int[n];
-        Arrays.fill(dp,-1);
-        System.out.println(ClimbStairsWithVariableJumps_Memo(0, n, moves,dp));   
-     
-    }  
-    public static int ClimbStairsWithVariableJumps_rec(int currStep,int totalStep,int moves[]){
-        if(currStep>totalStep) return 0;
-        if(currStep==totalStep) return 1;
-        int ans=0;
-        for(int i=1;i<=moves[currStep];i++){
-           ans += ClimbStairsWithVariableJumps_rec(currStep+i, totalStep, moves);
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int moves[] = new int[n];
+        for(int i = 0 ; i < n ; i++){
+            moves[i] = scn.nextInt();
+        }
+
+        // System.out.println(climbStairsVarJump_rec(0,n,moves));
+        int mem[] = new int[n+1];
+        Arrays.fill(mem,-1);
+        System.out.println(climbStairsVarJump_mem(0,n,moves,mem));
+    }
+    public static int climbStairsVarJump_rec(int currStep,int totalStep,int moves[]){
+        if(currStep == totalStep){
+            return 1;
+        }
+        int maxJump = moves[currStep];
+        int ans = 0;
+        for(int step = 1 ; step <= maxJump ; step++){
+            if(currStep + step <= totalStep){
+                ans += climbStairsVarJump_rec(currStep + step,totalStep,moves);
+            }
         }
         return ans;
-    } 
-    public static int ClimbStairsWithVariableJumps_Memo(int currStep,int totalStep,int moves[],int []dp){       
-        if(currStep>totalStep) return 0;
-        if(currStep==totalStep) return 1;
-        if(dp[currStep]!=-1) return dp[currStep];
-        int ans=0;
-        for(int i=1;i<=moves[currStep];i++){
-           ans += ClimbStairsWithVariableJumps_Memo(currStep+i, totalStep, moves,dp);
+    }
+    public static int climbStairsVarJump_mem(int currStep,int totalStep,int moves[],int mem[]){
+        if(currStep == totalStep){
+            return mem[currStep] = 1;
         }
-        return dp[currStep]=ans;
-    } 
-    
+        if(mem[currStep] != -1){
+            return mem[currStep];
+        }
+        int maxJump = moves[currStep];
+        int ans = 0;
+        for(int step = 1 ; step <= maxJump ; step++){
+            if(currStep + step <= totalStep){
+                ans += climbStairsVarJump_mem(currStep + step,totalStep,moves,mem);
+            }
+        }
+        return mem[currStep] = ans;
+    }
+
 }
